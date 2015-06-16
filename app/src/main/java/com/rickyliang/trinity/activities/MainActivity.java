83f8d8f1e0244fef44394018712be08d50219cc8
Ricky Liang <rickyliang@berkeley.edu>
2015-06-16 01:30:55 -0400

    Update testing environment structure

--- app/src/main/java/com/rickyliang/trinity/activities/MainActivity.java
+++ app/src/main/java/com/rickyliang/trinity/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.rickyliang.trinity;
+package com.rickyliang.trinity.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.rickyliang.trinity.R;
 
 
 public class MainActivity extends ActionBarActivity {
